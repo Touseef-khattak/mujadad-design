891ab76239af14a4f842839353e1b0878c4b0721
HamzaCoder020 <hamza40javed@gmail.com>
2023-11-10 12:37:14 +0500

    FILES CLEANSING

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer data-scroll-section data-cursor-white="true" id="footer" style="">
     <div class="container" data-scroll data-scroll-speed="0">
-       
+
         <div class="row">
             <div class="col-md-12  pe-md-0">
                 <h3 class="sub-heading"><a class="animate_border" href="{{('contact')}}">Let's work together<br class="d-none d-md-block">Get in touch.</a></h3>
@@ -12,39 +12,21 @@
             </div>
             <div class="col-md-3">
                 <div class="social_links_footer w-100 justify-content-md-end d-md-flex d-none">
-                    <a class="ft_social_link animate_border my-3 my-md-0" target="_blank" href="https://www.linkedin.com/company/tangent-tek/mycompany/">LinkedIn</a>
-                    <a class="ft_social_link d-none animate_border my-3 my-md-0" target="_blank" href="https://www.facebook.com/TechTangent">Facebook</a>
-                    <a class="ft_social_link d-none animate_border my-3 my-md-0" target="_blank" href="https://twitter.com/tangenttek_">Twitter</a>
+                    <a class="ft_social_link animate_border my-3 my-md-0" target="_blank" href="https://www.linkedin.com/company/">LinkedIn</a>
+                    <a class="ft_social_link d-none animate_border my-3 my-md-0" target="_blank" href="https://www.facebook.com">Facebook</a>
+                    <a class="ft_social_link d-none animate_border my-3 my-md-0" target="_blank" href="https://twitter.com/">Twitter</a>
                 </div>
                 <div class="social_links_footer mt-5 mb-3 w-100 d-flex justify-content-center d-md-none">
-                    <a class="ft_social_link" href="https://www.linkedin.com/company/tangent-tek/mycompany/">
-                        <img class="d-md-none d-block mx-auto" width="24" src="{{asset('assets/images/linkedin.png')}}" alt="TANGENTTEK LINKEDIN">
+                    <a class="ft_social_link" href="https://www.linkedin.com/">
+                        <img class="d-md-none d-block mx-auto" width="24" src="{{asset('assets/images/linkedin.png')}}" alt="MUJJADDAD LINKEDIN">
                     </a>
-                    <a class="ft_social_link" href="https://www.facebook.com/TechTangent">
-                        <img class="d-md-none d-block mx-auto" width="24" src="{{asset('assets/images/facebook.png')}}" alt="TANGENTTEK FACEBOOK">
+                    <a class="ft_social_link" href="https://www.facebook.com/">
+                        <img class="d-md-none d-block mx-auto" width="24" src="{{asset('assets/images/facebook.png')}}" alt="MUJJADDAD FACEBOOK">
                     </a>
-                    <a class="ft_social_link" href="https://twitter.com/tangenttek_">
-                        <img class="d-md-none d-block mx-auto" width="24" src="{{asset('assets/images/twitter.png')}}" alt="TANGENTTEK TWITRER">
+                    <a class="ft_social_link" href="https://twitter.com/">
+                        <img class="d-md-none d-block mx-auto" width="24" src="{{asset('assets/images/twitter.png')}}" alt="MUJJADDAD TWITRER">
                     </a>
                 </div>
-            </div>
-        </div>
-        <div class="row d-none">
-            <div class="col-md-3">
-                <p class="sub-para px-4 px-md-0">
-                    400 Renaissance Center, Suite 2600 Detroit, MI 48243, US
-                </p>
-            </div>
-            <div class="col-md-3 my-4 my-md-0">
-                <p class="sub-para px-4 px-md-0">
-                    397 Service Rd E, I-9/3,
-                    Islamabad Capital Territory
-                </p>
-            </div>
-            <div class="col-md-3">
-                <p class="sub-para px-4 px-md-0">
-                    c-2-15, coPlace 1, 2270 jalan Usahawan2, 63000 Cyberjaya, Darul Ehsan, Malaysia
-                </p>
             </div>
         </div>
     </div>
